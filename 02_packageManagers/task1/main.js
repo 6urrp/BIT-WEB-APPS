1de2d8543876ecb302d6b5f07540e191ab71b502
6urrp <sndrtasic@gmail.com>
2021-08-09 20:57:38 +0200

    Using npm through functions/Changing figlet logic with promises

--- 02_packageManagers/task1/main.js
+++ 02_packageManagers/task1/main.js
@@ -2,50 +2,95 @@
 
 let funnyWords = require('funny-words');
 
-console.log(funnyWords("I need to live a thousand times"));
-console.log(funnyWords("Sorry, you can see it in my retina"));
+const funny = () => {
+    let getFunnyWords = funnyWords("I need to live a thousand times");
+    return getFunnyWords;
+}
+const getFunny = funny();
+console.log(getFunny);
+
+const funny2 = () => {
+    let getFunnyWords = funnyWords("Sorry, you can see it in my retina");
+    return getFunnyWords;
+}
+const getFunny2 = funny2();
+console.log(getFunny2);
 
 //one line joke
 
 let oneLinerJoke = require('one-liner-joke');
-let getRandomJoke = oneLinerJoke.getRandomJoke();
-let getRandomJoke2 = oneLinerJoke.getRandomJoke();
-console.log(getRandomJoke.body);
-console.log(getRandomJoke2.body);
+
+const oneLiner = () => {
+    let getRandomJoke = oneLinerJoke.getRandomJoke();
+    return getRandomJoke.body;
+}
+const randomJoke = oneLiner();
+console.log(randomJoke);
+
+const oneLiner2 = () => {
+    let getRandomJoke = oneLinerJoke.getRandomJoke();
+    return getRandomJoke.body;
+}
+const randomJoke2 = oneLiner2();
+console.log(randomJoke2);
+
 
 //random words 
 
 let randomWords = require('random-words');
-console.log(randomWords(3).join(" is random word, "));
-console.log(randomWords(3))
+
+const favoriteWords = () => {
+    let getRandomWords =  randomWords(3);
+    return `My favorite words in english are ${getRandomWords.join(", ")}`
+}
+const favorite = favoriteWords();
+console.log(favorite)
 
 //figlet 
 
 let figlet = require('figlet');
-figlet("Mr. Hollywood", function(error, data) {
-    if (error) {
-        console.log("Oops, something went wrong!")
-        console.dir(err);
-        return;
-    }
-    console.log(data);
-});
+
+const figlet1 = data => {
+    return new Promise((fullfil, reject) => {
+        figlet.text(data, {
+            font: 'Sub-Zero',
+            horizontalLayout: 'default',
+            verticalLayout: 'default',
+            width: 200,
+            whitespaceBreak: true
+        }, function(err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                fullfil(data);
+            }
+        })
+    })
+}
+
+figlet1("Hello World").then(function(result) {
+    console.log(result);
+})
 
 
-figlet.text('Hello World', {
-    font: 'Sub-Zero',
-    horizontalLayout: 'default',
-    verticalLayout: 'default',
-    width: 200,
-    whitespaceBreak: true
-}, function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data);
-});
+let figlet = require('figlet');
+
+const figlet2 = data => {
+    return new Promise((fullfil, reject) => {
+        figlet(data, function(err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                fullfil(data);
+            }
+        });
+    });
+}
+
+figlet2("Mr.Hollywood").then(function(result) {
+    console.log(result);
+})
+
 
 figlet.text('Hello World', {
     font: 'Slant',
@@ -65,8 +110,20 @@ figlet.text('Hello World', {
 //cowsay
 
 let cowsay = require("cowsay");
-console.log(cowsay.say({
-    text : "Don't got that much in the bank, we go out she order the steak",
-    e : "oO",
-    T : "U "
-}));
+
+const speakingCow = () => {
+    let muu = cowsay.say({
+        text : "Don't got that much in the bank, we go out she order the steak",
+        e : "oO",
+        T : "U "
+    });
+    return `I have a cow that always says what I don't want to hear!!
+
+    ${muu};
+    
+
+
+    Told ya, she is unberable!!`
+}
+const myCow = speakingCow();
+console.log(myCow);
